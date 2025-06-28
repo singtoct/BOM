f6f1a6be916aee5d84d899ec74175edfd30a9f94
singtoct <singtoct@gmail.com>
2025-06-28 22:11:49 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BomProvider } from './context/BomContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BomProvider>
