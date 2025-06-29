1496b2dd504bed554f6683cc618ba9b72f6199a4
singtoct <singtoct@gmail.com>
2025-06-29 10:18:19 +0700

    1018

--- vite.config.ts
+++ vite.config.ts
@@ -27,4 +27,11 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: [
+      // This is a workaround for the strange Vercel build error.
+      // It explicitly tells Rollup how to resolve the module.
+      { find: 'react-dom-client', replacement: 'react-dom/client' }
+    ]
+  }
 })
